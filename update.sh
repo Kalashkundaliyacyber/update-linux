a1080e5786d39ecbb37e8a275d742a529b658e0a
kalashkundaliyacyber <kalashkundaliya@gmail.com>
2025-01-22 13:23:07 +0530

    add apt-get

--- update.sh
+++ update.sh
@@ -7,20 +7,38 @@ echo "Starting system update and maintenance..."
 # Update package list
 sudo apt update
 
+# Update package list
+sudo apt-get update
+
 # Upgrade installed packages
 sudo apt upgrade -y
+
+# Upgrade installed packages
+sudo apt-get upgrade -y
 
 # Perform a distribution upgrade (for newer versions of packages)
 sudo apt dist-upgrade -y
 
+# Perform a distribution upgrade (for newer versions of packages)
+sudo apt-get dist-upgrade -y
+
 # Remove unnecessary packages and dependencies
 sudo apt autoremove -y
+
+# Remove unnecessary packages and dependencies
+sudo apt-get autoremove -y
 
 # Clean up unnecessary cached files from the package manager
 sudo apt autoclean -y
 
+# Clean up unnecessary cached files from the package manager
+sudo apt-get autoclean -y
+
 # Fix broken dependencies (if any)
 sudo apt --fix-broken install
+
+# Fix broken dependencies (if any)
+sudo apt-get --fix-broken install
 
 # Clear package manager logs to free up disk space
 sudo journalctl --vacuum-time=2weeks
